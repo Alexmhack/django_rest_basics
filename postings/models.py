77dc2c23b291498b8459d6b53f9fd29ebca47153
alexHooper <alexmohack@gmail.com>
2018-10-14 19:38:18 +0530

    create blog model

--- postings/models.py
+++ postings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+class Blog(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.CharField(max_length=120, null=True, blank=True)
+	content = models.TextField(max_length=120, null=True, blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.user
