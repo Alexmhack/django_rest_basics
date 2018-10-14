d578a3cb831921d1157c9b3914d5a6aeb894d64d
alexHooper <alexmohack@gmail.com>
2018-10-14 20:35:02 +0530

    add validate method on title

--- postings/api/serializers.py
+++ postings/api/serializers.py
@@ -6,5 +6,17 @@ class BlogSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Blog
 		fields = ('pk', 'user', 'title', 'content', 'timestamp')
+		read_only_fields = ('user',)
 
 		# serializer converts to json and validates the data
+		
+	# validate_<field_name>
+	def validate_title(self, value):
+		qs = Blog.objects.filter(title__iexact=value)
+
+		# excludes the instance
+		if self.instance:
+			qs = qs.exclude(pk=self.instance.pk)
+		if qs.exists():
+			raise serializers.ValidationError("This title has already been used")
+		return value
