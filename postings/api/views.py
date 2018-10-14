d98a62608114fdaff45a3b56c901de6f33d60fed
alexHooper <alexmohack@gmail.com>
2018-10-14 20:03:08 +0530

    add permission in rest view

--- postings/api/views.py
+++ postings/api/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from postings.models import Blog
 from .serializers import BlogSerializer
 
 class BlogRUDView(generics.RetrieveUpdateDestroyAPIView):
+	# field which is looked by serializer while rendering json
 	lookup_field = "pk"
 	queryset = Blog.objects.all().order_by('-timestamp')
 	serializer_class = BlogSerializer
+	permission_classes = (IsAuthenticated,)
 
 	# def get_queryset(self):
 	# 	return Blog.objects.all()
