4db9f3ed2c909d969a7bbe65dd33c706e4158d33
alexHooper <alexmohack@gmail.com>
2018-10-14 20:34:38 +0530

    use other generic views

--- postings/api/views.py
+++ postings/api/views.py
@@ -5,6 +5,7 @@ from postings.models import Blog
 from .serializers import BlogSerializer
 
 class BlogRUDView(generics.RetrieveUpdateDestroyAPIView):
+	# RetrieveUpdateDestroyAPIView / RetrieveAPIView
 	# field which is looked by serializer while rendering json
 	# lookup_field = "pk"	"pk" is the default lookup field in generic view
 	queryset = Blog.objects.all().order_by('-timestamp')
