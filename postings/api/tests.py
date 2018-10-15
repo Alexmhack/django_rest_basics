154ff1d96dc7f108cf52e799de998bb72cdc03f4
alexHooper <alexmohack@gmail.com>
2018-10-15 17:02:25 +0530

    create tests for api

---
+++ postings/api/tests.py
@@ -0,0 +1,22 @@
+from rest_framework.test import APITestCase
+
+from django.contrib.auth import get_user_model
+
+from postings.model import Blog
+
+User = get_user_model()
+
+class BlogAPITestCase(APITestCase):
+	def setUp(self):
+		user = User.objects.create(username='testcase', email='testcase@django.com')
+		user.set_password('thisistestpass')
+		user.save()
+		blog_post = Blog.objects.create(
+			user=user,
+			title='Test Title',
+			content='Test Content'
+		)
+
+	def test_single_user(self):
+		user_count = User.objects.count()
+		self.assertEqual(user_count, 1)
