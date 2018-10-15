d2da19cdcad739439810a3e984e722b623c0f69e
alexHooper <alexmohack@gmail.com>
2018-10-15 17:37:54 +0530

    change url name for list api

--- postings/api/urls.py
+++ postings/api/urls.py
@@ -5,7 +5,7 @@ from .views import (BlogRUDView, BlogListAPIView)  # , BlogCreateAPIView)
 app_name = 'api-postings'
 
 urlpatterns = [
-	path('', BlogListAPIView.as_view(), name='blog-list'),
+	path('', BlogListAPIView.as_view(), name='blog-list-create'),
 	path('<int:pk>', BlogRUDView.as_view(), name='blog-rud'),
 	# path('create/', BlogCreateAPIView.as_view(), name='blog-create'),
 ]
